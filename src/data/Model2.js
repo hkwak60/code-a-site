20ceb6c5b5579c034df28499ec15fee28eae23d2
Sooa Kim <sooa.kim@stonybrook.edu>
2025-04-12 13:25:34 -0400

    photo upload

---
+++ src/data/Model2.js
@@ -0,0 +1,245 @@
+export default class Model {
+  constructor() {
+    this.data = {
+      Nebraska: [
+        {
+          park: 'Agate Fossil Beds National Monument',
+          image: '',
+          description: 'Preserves Miocene-era fossil beds and Native American artifacts.',
+        },
+        {
+          park: 'Scotts Bluff National Monument',
+          image: ['public/images/D52C8F33-0529-96EC-76CEC05B91A4A6ED'],
+          description: 'Historic landmark along the Oregon Trail with towering bluffs.',
+        },
+      ],
+      Nevada: [
+        {
+          park: 'Great Basin National Park',
+          image: '',
+          description: 'Features ancient bristlecone pines and the Lehman Caves.',
+        },
+      ],
+      NewHampshire: [
+        {
+          park: 'Saint-Gaudens National Historical Park',
+          image: '',
+          description: 'Home and studios of sculptor Augustus Saint-Gaudens.',
+        },
+      ],
+      NewJersey: [
+        {
+          park: 'Thomas Edison National Historical Park',
+          image: '',
+          description: 'Preserves the laboratory and residence of inventor Thomas Edison.',
+        },
+        {
+          park: 'Delaware Water Gap National Recreation Area',
+          image: '',
+          description: 'Offers scenic river valleys and forested mountains along the Delaware River.',
+        },
+      ],
+      NewMexico: [
+        {
+          park: 'Carlsbad Caverns National Park',
+          image: '',
+          description: 'Renowned for its vast underground limestone chambers.',
+        },
+        {
+          park: 'White Sands National Park',
+          image: '',
+          description: 'Features expansive dunes of white gypsum sand.',
+        },
+      ],
+      NewYork: [
+        {
+          park: 'Gateway National Recreation Area',
+          image: '',
+          description: 'Urban park offering beaches, wildlife habitats, and historic sites.',
+        },
+        {
+          park: 'Saratoga National Historical Park',
+          image: '',
+          description: 'Site of the pivotal 1777 Revolutionary War battle.',
+        },
+      ],
+      NorthCarolina: [
+        {
+          park: 'Great Smoky Mountains National Park',
+          image: '',
+          description: 'Known for its biodiversity and mist-covered mountains.',
+        },
+        {
+          park: 'Blue Ridge Parkway',
+          image: '',
+          description: 'Scenic drive through the Appalachian Highlands.',
+        },
+      ],
+      NorthDakota: [
+        {
+          park: 'Theodore Roosevelt National Park',
+          image: '',
+          description: 'Badlands terrain honoring the conservationist president.',
+        },
+      ],
+      Ohio: [
+        {
+          park: 'Cuyahoga Valley National Park',
+          image: '',
+          description: 'Protects rural landscapes along the Cuyahoga River.',
+        },
+      ],
+      Oklahoma: [
+        {
+          park: 'Chickasaw National Recreation Area',
+          image: '',
+          description: 'Known for its mineral springs and recreational lakes.',
+        },
+        {
+          park: 'Washita Battlefield National Historic Site',
+          image: '',
+          description: 'Commemorates the 1868 attack on a Cheyenne village.',
+        },
+      ],
+      Oregon: [
+        {
+          park: 'Crater Lake National Park',
+          image: '',
+          description: 'Home to the deepest lake in the U.S., formed in a volcanic caldera.',
+        },
+      ],
+      Pennsylvania: [
+        {
+          park: 'Valley Forge National Historical Park',
+          image: '',
+          description: 'Encampment site of the Continental Army during the Revolutionary War.',
+        },
+      ],
+      RhodeIsland: [
+        {
+          park: '',
+          image: ['public/images/RI-CTA-tncrhodeisland_93711283'],
+          description: 'Honors the founder of Rhode Island and advocate for religious freedom.',
+        },
+        {
+          park: 'Roger Williams National Memorial',
+          image: ['public/images/'],
+          description: 'Honors the founder of Rhode Island and advocate for religious freedom.',
+        },
+        
+      ],
+      SouthCarolina: [
+        {
+          park: 'Congaree National Park',
+          image: ['public/images/37147B45-1DD8-B71C-077C4B1909C0918BOriginal', 'public/images/378A474F-1DD8-B71C-07F6C2499723614DOriginal', 'public/images/36D3619A-1DD8-B71C-07055020D045992BOriginal'],
+          description: 'Protects the largest expanse of old-growth bottomland hardwood forest in the U.S.',
+        },
+      ],
+      SouthDakota: [
+        {
+          park: 'Wind Cave National Park',
+          image: ['public/images/9472010a-e28a-4d99-be3f-d22984b13947Original', 'public/images/91702e6d-a85f-4346-b548-a99ae994c0ebOriginal', 'public/images/11e65e85-6f8d-4c10-b28b-c44974e646acOriginal'],
+          description: 'Known for its intricate cave system and mixed-grass prairie.',
+        },
+      ],
+      Tennessee: [
+        {
+          park: 'Great Smoky Mountains National Park',
+          image: ['public/images/e47c93eb-29da-4cdd-9d30-4fcee8e144eaOriginal', 'public/images/2cc5f989-a181-402a-a79b-bae9ceefe2c6Original'],
+          description: 'Renowned for its diverse wildlife and misty mountain vistas.',
+        },
+      ],
+      Texas: [
+        {
+          park: 'Big Bend National Park',
+          image: '',
+          description: ['public/images/f2f14d38-726f-4e67-9a0d-225e5ae593c9Original', 'public/images/2F818546-155D-451F-67B8EE1E8AD16041Original', 'public/images/ED89E87C-155D-451F-674819921EE03FC8Original'],
+        },
+        {
+          park: 'Guadalupe Mountains National Park',
+          image: '',
+          description: 'Home to the highest peak in Texas and fossil-rich mountains.',
+        },
+      ],
+      Utah: [
+        {
+          park: 'Arches National Park',
+          image: ['public/images/67937551-7ac6-47ef-87ae-885c132218e2Original', 'public/images/202DA03A-155D-451F-6771F1DDDC603093Original', 'public/images/d747268c-b693-4043-8f57-45d4bb119e9bOriginal'],
+          description: 'Contains over 2,000 natural sandstone arches.',
+        },
+        {
+          park: 'Bryce Canyon National Park',
+          image: ['public/images/'],
+          description: 'Famous for its unique hoodoo rock formations.',
+        },
+        {
+          park: 'Canyonlands National Park',
+          image: ['public/images/'],
+          description: 'Features dramatic desert landscape carved by the Colorado River.',
+        },
+        {
+          park: 'Capitol Reef National Park',
+          image: ['public/images/'],
+          description: 'Known for its colorful canyons and the Waterpocket Fold.',
+        },
+        {
+          park: 'Zion National Park',
+          image: ['public/images/'],
+          description: 'Offers towering sandstone cliffs and narrow slot canyons.',
+        },
+      ],
+      Vermont: [
+        {
+          park: 'Marsh-Billings-Rockefeller National Historical Park',
+          image: ['public/images/60B192AE-1DD8-B71B-0B004467B8FF638DOriginal', 'public/images/2E3049DA-1DD8-B71B-0BBCD6526FDFF299Original'],
+          description: 'Focuses on conservation history and sustainable land stewardship.',
+        },
+      ],
+      Virginia: [
+        {
+          park: 'Shenandoah National Park',
+          image: ['public/images/C6226B77-1DD8-B71B-0B437B66109591DDOriginal', 'public/images/C6D6EA0B-1DD8-B71B-0B389271E74E52F1Original', 'public/images/5468F2A6-155D-451F-67DBC3A3F8C721BEOriginal'],
+          description: 'Features forested mountains and the scenic Skyline Drive.',
+        },
+      ],
+      Washington: [
+        {
+          park: 'Mount Rainier National Park',
+          image: ['public/images/92941B82-B71E-0320-F391BF5AC26AAFABOriginal'],
+          description: 'Dominated by its namesake glacier-capped volcano.',
+        },
+        {
+          park: 'North Cascades National Park',
+          image: ['public/images/927C0F8F-A841-CAB1-4F8ED7828A8B5EA5Original', 'public/images/670C23BF-1DD8-B71B-0B6D2A7EC83F1963LoResProxy'],
+          description: 'Known for rugged peaks and numerous glaciers.',
+        },
+      ],
+      WestVirginia: [
+        {
+          park: 'New River Gorge National Park and Preserve',
+          image: ['public/images/C2EF416C-1DD8-B71B-0BC241739294EB03Original', 'public/images/3f422525-8835-462a-8cbf-8d701c066ac0Original', 'public/images/622B5627-155D-451F-674BA8A3B60EF98DOriginal'],
+          description: '',
+        },
+      ],
+      Wisconsin: [
+        {
+          park: 'Apostle Islands National Lakeshore',
+          image: ['public/images/Apostle-Islands-Cornucopia-Wi2-Chad-Briesemeister-scaled', 'public/images/Apostle-Islands-WI-Lorinda-Niemi', 'public/images/ white-pines.-great-rocks-for-jumping-in-apostle-islands'],
+          description: '',
+        },
+      ],
+      Wyoming: [
+        {
+          park: 'Grand Teton National Park',
+          image: ['public/images/2880x1620-featured-image-grand-tetons-scaled'],
+          description: '',
+        },
+        {
+          park: 'Yellowstone National Park',
+          image: ['public/images/Yellowstone+Magma_hero', 'public/images/Yellowstone_River_in_Hayden_Valley', 'public/images/07_yellowstone'],
+          description: '',
+        },
+      ],
+    };
+  }
+}
