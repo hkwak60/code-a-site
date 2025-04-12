2d56130d42e780b62cc744551d71282c6481074f
dahyun-eom <dahyuneom@gmail.com>
2025-04-12 12:00:14 -0400

    model

---
+++ src/data/Model1.js
@@ -0,0 +1,14 @@
+export default class Model {
+  constructor() {
+    this.data = {
+      Arizona: [
+        //array of community objects
+        {
+          park: 'Grand Canyon',
+          image: '',
+          description: 'A practical application of the principles of justice.',
+        },
+      ],
+    }; //end this.data object
+  } // end constructor()
+} // end Model
